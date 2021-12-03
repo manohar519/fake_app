ccb08647e5b8952d26fda4cdd1d5f18f4cc305f6
manohar519 <chimatamanohar519@gmail.com>
2021-12-03 20:33:40 +0530

    Updates

---
+++ precache-manifest.73c42810f8e36630dfea98f875cf16c5.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "f279a152d1cb10a193297edc3371701a",
+    "url": "/fake_app/index.html"
+  },
+  {
+    "revision": "275453c737debf13d62b",
+    "url": "/fake_app/static/css/2.58570433.chunk.css"
+  },
+  {
+    "revision": "902a5cbdf20b72c39154",
+    "url": "/fake_app/static/css/main.0d2bfa13.chunk.css"
+  },
+  {
+    "revision": "275453c737debf13d62b",
+    "url": "/fake_app/static/js/2.bad77cdb.chunk.js"
+  },
+  {
+    "revision": "902a5cbdf20b72c39154",
+    "url": "/fake_app/static/js/main.f9b2fdbd.chunk.js"
+  },
+  {
+    "revision": "4c8f5c1a08e503b24467",
+    "url": "/fake_app/static/js/runtime~main.08fa1813.js"
+  },
+  {
+    "revision": "9f7e506d7455da27ce24e474c84a8e0a",
+    "url": "/fake_app/static/media/logo.9f7e506d.svg"
+  }
+]);
